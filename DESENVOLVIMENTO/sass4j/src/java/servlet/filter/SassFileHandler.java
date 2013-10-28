2c21af3346a8feef529f941664612f5f215454ff
Guilherme Morgado <gcmorgado@gmail.com>
2013-10-28 14:23:49 -0200

    file handler added

---
+++ DESENVOLVIMENTO/sass4j/src/java/servlet/filter/SassFileHandler.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet.filter;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ *
+ * @author gmorgado
+ */
+public class SassFileHandler {
+    
+    public File readFile(File file) throws IOException {
+        
+        FileWriter fw = new FileWriter(file);
+        BufferedWriter bw = new BufferedWriter(fw);
+        FileReader fr = new FileReader(file);
+        fr.
+        
+        return file;
+        
+    }
+
+    
+    
+    
+    
+    
+}
