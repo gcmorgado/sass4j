25d1bc4e34be0d715fb673697cdda3abd64736ce
Guilherme Morgado <gcmorgado@gmail.com>
2013-10-28 13:54:25 -0200

    filehandler added

--- DESENVOLVIMENTO/sass4j/src/java/servlet/filter/SassFilter.java
+++ DESENVOLVIMENTO/sass4j/src/java/servlet/filter/SassFilter.java
@@ -1,12 +1,15 @@
 package servlet.filter;
 
+import java.io.File;
 import java.io.IOException;
+import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -15,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
  * @author gmorgado
  */
 
+@WebFilter(urlPatterns = "*.css", dispatcherTypes = DispatcherType.REQUEST)
 public class SassFilter implements Filter {
 
     @Override
@@ -26,15 +30,22 @@ public class SassFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException { 
         
+        SassFileHandler sfh = new SassFileHandler();
+        
         if(request instanceof HttpServletRequest && response instanceof HttpServletResponse) { 
             HttpServletRequest httpReq = (HttpServletRequest) request;
             HttpServletResponse httpResp = (HttpServletResponse) response; 
             String fileName = httpReq.getRequestURI().substring(httpReq.getRequestURI().lastIndexOf("/"), httpReq.getRequestURI().length());
             if (fileName.toLowerCase().endsWith("css")) { 
-                httpResp.setContentType("text/css"); 
+                //httpResp.setContentType("text/css"); 
+                sfh.readFile();
             } 
-            httpResp.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\""); 
+            //httpResp.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\""); 
         } 
+        
+        
+        
+        
         chain.doFilter(request, response);  
     }
 
