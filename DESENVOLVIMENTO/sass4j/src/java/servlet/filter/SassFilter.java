3567ac5aa2a69c6719bc9613ae91307fa48be9f9
Guilherme Morgado <gcmorgado@gmail.com>
2014-06-09 23:03:47 -0300

    no message

--- DESENVOLVIMENTO/sass4j/src/java/servlet/filter/SassFilter.java
+++ DESENVOLVIMENTO/sass4j/src/java/servlet/filter/SassFilter.java
@@ -6,7 +6,10 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
@@ -42,54 +45,63 @@ public class SassFilter implements Filter {
     
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException, FileNotFoundException {
-        
+
         HttpServletRequest httpReq = (HttpServletRequest) request;
         HttpServletResponse httpResp = (HttpServletResponse) response;
         String sassPath = httpReq.getRequestURI().replace("/sass4j", "");
+        List<File> listOfFiles = new ArrayList<>();
         File cssFile = new File(httpReq.getServletContext().getRealPath(sassPath));
         File sassFile = new File(httpReq.getServletContext().getRealPath(sassPath).replace(".css", ".scss"));
-        httpResp.setHeader("Content-Type", "text/css");
-        Scanner scanSass = new Scanner(sassFile, "UTF-8");
-        StringBuilder sass = new StringBuilder(); 
-        while (scanSass.hasNextLine()) {
-            sass.append(scanSass.nextLine());
-        }
-        if (sassFile.exists()) {
-            if (cssFile.exists()) {
-                chain.doFilter(request, response); 
-            } else {
-                OutputStream os = httpResp.getOutputStream();
-                if(!cssMap.isEmpty() && !sassMap.isEmpty()) {
-                    if(sassMap.get(cssFile.toString()).equals(sass.toString())) {
-                        os.write(cssMap.get(cssFile.toString()).getBytes());
-                    }
+        listOfFiles.add(sassFile);
+        for (File f : listOfFiles) {
+            httpResp.setHeader("Content-Type", "text/css");
+            Scanner scanSass = new Scanner(sassFile, "UTF-8");
+            StringBuilder sass = new StringBuilder(); 
+            while (scanSass.hasNextLine()) {
+                sass.append(scanSass.nextLine());
+            }
+            if(sassFile.exists()) {
+                if(cssFile.exists()) {
+                    chain.doFilter(request, response); 
                 } else {
-                    RubyInstanceConfig config = new RubyInstanceConfig();   
-                    config.setCompatVersion(CompatVersion.RUBY2_0);
-                    String rubyFile = SassFilter.class.getResource("../../sass4j.rb").getFile();
-                    String sassScript = SassFilter.class.getResource("../../sass.rb").getFile();
-                    ScriptEngineManager manager = new ScriptEngineManager();
-                    ScriptEngine engine = manager.getEngineByName("jruby"); 
-                    InputStreamReader isr = new InputStreamReader(new FileInputStream(rubyFile), "UTF8");
-                    engine.put("sassScript", sassScript);
-                    try {
-                        engine.eval(isr);
-                    } catch (ScriptException ex) {
-                        throw new RuntimeException(ex);
-                    }
-                    Invocable inv = (Invocable) engine;
-                    try {
-                        Object ret = inv.invokeFunction("compile", sass.toString());
-                        byte[] bytes = ret.toString().getBytes();
-                        os.write(bytes);
-                        cssMap.put(cssFile.toString(), ret.toString());
-                        sassMap.put(cssFile.toString(), sass.toString());
-                    } catch (ScriptException | NoSuchMethodException ex) {
-                        throw new RuntimeException(ex);
+                    OutputStream os = httpResp.getOutputStream();
+                    if(sassMap.isEmpty() || cssMap.isEmpty()) {
+                        RubyInstanceConfig config = new RubyInstanceConfig();   
+                        config.setCompatVersion(CompatVersion.RUBY2_0);
+                        String rubyFile = SassFilter.class.getResource("../../sass4j.rb").getFile();
+                        String sassScript = SassFilter.class.getResource("../../sass.rb").getFile();
+                        ScriptEngineManager manager = new ScriptEngineManager();
+                        ScriptEngine engine = manager.getEngineByName("jruby"); 
+                        InputStreamReader isr = new InputStreamReader(new FileInputStream(rubyFile), "UTF8");
+                        engine.put("sassScript", sassScript);
+                        try {
+                            engine.eval(isr);
+                        } catch (ScriptException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                        Invocable inv = (Invocable) engine;
+                        try {
+                            Object ret = inv.invokeFunction("compile", sass.toString());
+                            byte[] bytes = ret.toString().getBytes();
+                            os.write(bytes);
+                            cssMap.put(f.toString(), ret.toString());
+                            sassMap.put(f.toString(), sass.toString());
+                        } catch (ScriptException | NoSuchMethodException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    } else {
+                        if(sassMap.get(f.toString()) != null) {
+                            if(sassMap.get(f.toString()).equals(sass.toString())) {
+                                os.write(cssMap.get(f.toString()).getBytes());
+                                System.out.println("cacheou");
+                            }
+                        }
                     }
                 }
             }
         }
+        
+        
     }
 
     @Override
