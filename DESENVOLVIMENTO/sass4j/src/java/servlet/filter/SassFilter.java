3ffcfa9a166c7f3c48f9548d8b00374740cbac7e
Guilherme Morgado <gcmorgado@gmail.com>
2014-06-10 21:28:59 -0300

    sass4j refatorado

--- DESENVOLVIMENTO/sass4j/src/java/servlet/filter/SassFilter.java
+++ DESENVOLVIMENTO/sass4j/src/java/servlet/filter/SassFilter.java
@@ -6,8 +6,11 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
@@ -33,18 +36,37 @@ import org.jruby.RubyInstanceConfig;
 @WebFilter(urlPatterns = "*.css", dispatcherTypes = DispatcherType.REQUEST)
 public class SassFilter implements Filter {
     
-    private final HashMap<String, String> cssMap = new HashMap();
-    private final HashMap<String, String> sassMap = new HashMap();
     private final HashMap<String, String[]> map = new HashMap();
     private final RubyInstanceConfig config;
     private final ScriptEngineManager manager;
-    private final ScriptEngine engine;
+    private final Invocable engine;
    
     public SassFilter() {
         config = new RubyInstanceConfig();   
         config.setCompatVersion(CompatVersion.RUBY2_0);
         manager = new ScriptEngineManager();
-        engine = manager.getEngineByName("jruby");                   
+        ScriptEngine script = manager.getEngineByName("jruby"); 
+        engine = (Invocable) script;
+        String rubyFile = SassFilter.class.getResource("../../sass4j.rb").getFile();
+        String sassScript = SassFilter.class.getResource("../../sass.rb").getFile();
+        script.put("sassScript", sassScript);
+        InputStreamReader isr;
+        try {
+            isr = new InputStreamReader(new FileInputStream(rubyFile), "UTF8");
+            script.eval(isr);
+        } catch (FileNotFoundException | UnsupportedEncodingException | ScriptException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+    
+    public String compileSass(StringBuilder sass) {
+        
+        try {
+            return engine.invokeFunction("compile", sass.toString()).toString();
+        } catch (ScriptException | NoSuchMethodException ex) {
+            throw new RuntimeException(ex);
+        }
+        
     }
     
     @Override
@@ -52,84 +74,39 @@ public class SassFilter implements Filter {
     }
     
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException, FileNotFoundException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
         HttpServletRequest httpReq = (HttpServletRequest) request;
         HttpServletResponse httpResp = (HttpServletResponse) response;
         String sassPath = httpReq.getRequestURI().replace("/sass4j", "");
         File cssFile = new File(httpReq.getServletContext().getRealPath(sassPath));
         
-        if(cssFile.exists()) {
+        if (cssFile.exists()) {
             chain.doFilter(request, response);
         } else {
             File sassFile = new File(httpReq.getServletContext().getRealPath(sassPath).replace(".css", ".scss"));
             if(sassFile.exists()) {
-                Scanner scanSass = new Scanner(sassFile, "UTF-8");
-                StringBuilder sass = new StringBuilder(); 
-                while (scanSass.hasNextLine()) {
-                    sass.append(scanSass.nextLine());
-                }
                 OutputStream os = httpResp.getOutputStream();
-                // TODO SISTEMA DE CACHE
-                if(!cssMap.isEmpty() && !sassMap.isEmpty()) {
-                    if(sassMap.get(cssFile.toString()).equals(sass.toString())) {
-                        os.write(cssMap.get(cssFile.toString()).getBytes());
-                        os.flush();
-                        os.close();
-                    }
+                byte[] bytesCss;
+                if(map.get(cssFile.toString())[0].equals(String.valueOf(sassFile.lastModified()))) {
+                    bytesCss = map.get(cssFile.toString())[1].getBytes();   
                 } else {
-                    
+                    Scanner scanSass = new Scanner(sassFile, "UTF-8");
+                    StringBuilder sass = new StringBuilder(); 
+                    while (scanSass.hasNextLine()) {
+                        sass.append(scanSass.nextLine());
+                    }
+                    String sassCompilado = compileSass(sass);
+                    bytesCss = sassCompilado.getBytes();
+                    map.put(cssFile.toString(), new String[]{String.valueOf(sassFile.lastModified()), sassCompilado});
                 }
+                httpResp.setHeader("Content-Type", "text/css");
+                os.write(bytesCss);
+                os.flush();
+                os.close();
             } else {
                 httpResp.sendError(404, "Arquivos SCSS e CSS não foram encontrado.");
             }
-        }
-        
-        
-        
-        
-        Scanner scanSass = new Scanner(sassFile, "UTF-8");
-        StringBuilder sass = new StringBuilder(); 
-        while (scanSass.hasNextLine()) {
-            sass.append(scanSass.nextLine());
-        }
-        if (sassFile.exists()) {
-            if (cssFile.exists()) {
-                chain.doFilter(request, response); 
-            } else {
-                OutputStream os = httpResp.getOutputStream();
-                if(!cssMap.isEmpty() && !sassMap.isEmpty()) {
-                    if(sassMap.get(cssFile.toString()).equals(sass.toString())) {
-                        os.write(cssMap.get(cssFile.toString()).getBytes());
-                        os.flush();
-                        os.close();
-                    }
-                } else {  
-                    String rubyFile = SassFilter.class.getResource("../../sass4j.rb").getFile();
-                    String sassScript = SassFilter.class.getResource("../../sass.rb").getFile();
-                    InputStreamReader isr = new InputStreamReader(new FileInputStream(rubyFile), "UTF8");
-                    engine.put("sassScript", sassScript);
-                    try {
-                        engine.eval(isr);
-                    } catch (ScriptException ex) {
-                        throw new RuntimeException(ex);
-                    }
-                    Invocable inv = (Invocable) engine;
-                    try {
-                        Object ret = inv.invokeFunction("compile", sass.toString());
-                        byte[] bytes = ret.toString().getBytes();
-                        httpResp.setHeader("Content-Type", "text/css");
-                        os.write(bytes);
-                        os.flush();
-                        os.close();
-                        cssMap.put(cssFile.toString(), ret.toString());
-                        sassMap.put(cssFile.toString(), sass.toString());
-                    } catch (ScriptException | NoSuchMethodException ex) {
-                        throw new RuntimeException(ex);
-                    }
-                }
-            }
-            
         }
     }
 
